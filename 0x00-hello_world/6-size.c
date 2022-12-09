fb528c081ea32a09386a061b7911acf23c13409c
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2022-12-08 16:52:32 -0800

    Add files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	int v_int;
+	long int v_lng_int;
+	long long int v_lng_lng_int;
+	char v_char;
+	float v_float;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v_char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(v_int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(v_lng_int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(v_lng_lng_int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v_float));
+	return (0);
+}
