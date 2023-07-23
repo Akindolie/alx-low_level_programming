dc815f873cea95250dd736bff34bd8de2853c64b
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-07-23 18:36:40 +0100

    Add files via upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints _putchar, followed by a new line
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char word[10] = "_putchar";
+
+	printf("%s\n", word);
+
+	return (0);
+}
