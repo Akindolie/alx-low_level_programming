946c0ed40fd95e0ef97e9526384e4deb31e4ad46
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-07-25 21:59:53 +0100

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - Prints out the first half of a string.
+ * @str: input string to print.
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
