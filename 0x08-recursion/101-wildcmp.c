83e5f407e43521ae2517c1cac0998cbc54a5d647
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-26 22:34:58 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,27 @@
-dchdvdj
+#include "main.h"
+/**
+ * wildcmp - compare two strings
+ * @firstString: first string parameter pointer
+ * @secondString: second string parameter pointer
+ * Return: 0
+ */
+ int wildcmp(char *firstString, char *secondString)
+ {
+	 if (*firstString == '\0')
+	 {
+		 if (*secondString != '\0' && *secondString == '*')
+		 {
+			 return (wildcmp(firstString, secondString + 1));
+		 }
+		 return (*secondString == '\0');
+	 }
+	 if (*secondString == '*')
+	 {
+		 return (wildcmp(firstString + 1, secondString) || wildcmp(firstString, secondString + 1));
+	 }
+	 else if (*firstString == *secondString)
+	 {
+		 return (wildcmp(firstString + 1, secondString +1));
+	 }
+	 return (0);
+ }
