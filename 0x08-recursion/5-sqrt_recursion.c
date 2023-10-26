9eb3be24bf41ec7a568c480798a0dfca4568d7a0
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-26 16:38:14 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
+ * verifier - checks the input parameter from n to the base
+ * @n: number is squared and verified against the base
+ * @baseVal: base number to check for
+ * Return: the natural square root of the number base
+ */
+int verifier(int n, int baseVal)
+{
+	if (n * n == baseVal)
+		return (n);
+	if (n * n > baseVal)
+		return (-1);
+	return (verifier(n + 1, baseVal));
+}
+/**
  * _sqrt_recursion - Calculate the natural square root of a number
  * @numb: input parameter
  * Return: the square root
  */
-int ch
+int _sqrt_recursion(int numb)
+{
+	return (verifier(1, numb));
+}
