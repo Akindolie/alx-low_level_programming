bfe47f4a6803bf49c8c65a5a2a01c8a0a575c40d
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-26 16:54:28 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
+#include "main.h"
 /**
-  * verifier - checks recursively the input from is_prime_number
-  * @n: an integer to iterate with
-  * @baseVal: baseVal number to check
-  * Return: 1 if n is a prime, else return 0 if n is not a prime.
-  */
-int verifier(int n, int baseVal)
+ * _strlen_recursion - returns the length of a input string.
+ * @strVal: the string parameter
+ * Return: length of the string parameter
+ */
+int _strlen_recursion(char *strVal)
 {
-	if (baseVal % n == 0 || baseVal < 2)
-		return (0);
-	else if (n == baseVal - 1)
-		return (1);
-	else if (baseVal > n)
-		return (verifier(n + 1, baseVal));
-	return (1);
+	if (*strVal)
+	{
+		strVal++;
+		return (1 + _strlen_recursion(strVal));
+	}
+	return (0);
 }
 /**
-  * is_prime_number - checks if a number is a prime number or not
-  * @numb: the number to check
-  * Return: 1 if numb is a prime, else return 0 if numb is not.
-  */
-int is_prime_number(int numb)
+ * verifier - func to help for is_palindrome
+ * @strVal: the string parameter
+ * @len: length of string parameter
+ * @counter: counter of recursion
+ * Return: returns 1 if string is a palindrome, 0 if it is not a palindrome.
+ */
+int verifier(char *strVal, int len, int counter)
 {
-	return (verifier(2, numb));
+	if (counter >= len)
+		return (1);
+	if (strVal[len] == strVal[counter])
+		return (verifier(strVal, len - 1, counter + 1));
+	return (0);
+}
+/**
+ * is_palindrome - checks if the input string is a palindrome
+ * @strVal: the string to check
+ * Return: 1 if string is a palindrome, 0 if it is not a palindrome.
+ */
+int is_palindrome(char *strVal)
+{
+	int len = _strlen_recursion(strVal);
+	int counter = 0;
+
+	return (verifier(strVal, len - 1, counter));
 }
