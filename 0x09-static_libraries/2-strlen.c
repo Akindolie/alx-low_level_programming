547776ac502c38bc26872cec97d1ab4d21cc5ef3
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:22:38 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,17 +1,20 @@
-/**
- * _strlen - calculates the length of a string.
- * @s: pointer to the character array.
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int i;
+#include "main.h"
 
-	i = 0;
-	while (*s != '\0')
+/**
+ * strlen - Calculates the length of an input string.
+ * @str: Pointer to the character array.
+ *
+ * Return: The calculated length of the string.
+ */
+int strlen(char *str)
+{
+	int length = 0;
+
+	while (*str != '\0')
 	{
-		i++;
-		s++;
+		length++;
+		str++;
 	}
-	return (i);
+
+	return (length);
 }
