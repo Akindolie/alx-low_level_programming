c24679530261b78cbb0361b34820775de35e5ecd
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:11:16 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,22 +1,25 @@
 /**
- * _strncat - concatenates two strings
- * @dest: pointer to the destination string
- * @src: pointer to the source string
- * @n: number of bytes to print
- * Return: pointer to the resulting string dest
+ * _strncat - Concatenate two strings with a limit on the number of characters copied.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: The maximum number of bytes to concatenate from src.
+ * Return: Pointer to the resulting destination string.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int dest_index = 0, src_index = 0;
 
-	i = 0;
-	for (j = 0; dest[j] != '\0'; j++)
-		;
-	while (src[i] != '\0' && i < n)
+	while (dest[dest_index] != '\0')
 	{
-		dest[j + i] = src[i];
-		i++;
+		dest_index++;
 	}
+
+	while (src[src_index] != '\0' && src_index < n)
+	{
+		dest[dest_index] = src[src_index];
+		dest_index++;
+		src_index++;
+	}
+
 	return (dest);
 }
