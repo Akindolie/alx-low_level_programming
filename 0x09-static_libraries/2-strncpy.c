ef35e24a99b3156e7a1fd1dcba6b74559dbd17ac
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:24:16 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,18 +1,20 @@
+#include "main.h"
+
 /**
- * _strncpy - copies a string up to n
- * @dest: Destination of the pointer to the string
- * @src: Source of the pointer to the string to copy
- * @n: Number of bytes to copy.
+ * _strncpy - Copies a string up to a specified length
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string to copy
+ * @n: Maximum number of bytes to copy
+ * 
  * Return: Pointer to the destination string.
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
-		dest[i]	= src[i];
-	while (i < n)
-		dest[i++] = '\0';
+	for (index = 0; src[index] != '\0' && index < n; index++)
+		dest[index] = src[index];
+	while (index < n)
+		dest[index++] = '\0';
 	return (dest);
 }
