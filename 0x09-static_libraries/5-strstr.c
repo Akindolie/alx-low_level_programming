a228d4ddef7c21dbc8b27567b261e80137b26076
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:40:19 +0100

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,42 +1,47 @@
-#include "holberton.h" 
+#include "main.h"
 #include <stdlib.h>
+
 /**
- * _strncmp - compares two strings within b bytes.
- * @s1: string to check
- * @s2: string to check from
- * @b: number of bytes to check
- * Return: 0 if strings are the same
- * any other number if they are different.
+ * _strncmp - Compare two strings within a specified number of bytes.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ * @n: The maximum number of bytes to compare.
+ * Return: 0 if the strings match within the specified bytes, else a non-zero value.
  */
-int _strncmp(char *s1, char *s2, unsigned int b)
+int _strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
-	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+
+	for (i = 0; i <= n && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
 		s1++;
 		s2++;
 	}
-	return (i - b);
+	return (i - n);
 }
+
 /**
- * _strstr - locates a substring
- * @haystack: string to look though
- * @needle: substring to find
- * Return: pointer to string with result of search
+ * _strstr - Locate a substring within a string.
+ * @haystack: The string to search within.
+ * @needle: The substring to find.
+ * Return: A pointer to the first occurrence of 'needle' in 'haystack', or NULL if not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int len;
-	/*Get length of needle for strncmp*/
-	len = 0;
-	while (needle[len] != '\0')
-		len++;
-	/*compare substring*/
+	unsigned int needle_len;
+
+	/* Determine the length of the 'needle' for _strncmp */
+	needle_len = 0;
+	while (needle[needle_len] != '\0')
+		needle_len++;
+
+	/* Search for the substring */
 	while (*haystack != '\0')
 	{
-		if (_strncmp(haystack, needle, len) == 0)
+		if (_strncmp(haystack, needle, needle_len) == 0)
 			return (haystack);
 		haystack++;
 	}
+
 	return (NULL);
 }
