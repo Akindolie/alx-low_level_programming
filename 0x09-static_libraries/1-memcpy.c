6dea15306931bcda15d0092dda40381a144dbb76
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:09:00 +0100

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,16 +1,16 @@
 /**
- * _memcpy - copies memory area
- * @dest: pointer to destination memory area
- * @src: pointer to source memory area
- * @n: number of bytes to copy
- * Return: pointer to destination memory area
+ * _memcpy - Copy memory from source to destination
+ * @dest: Pointer to the destination memory area
+ * @src: Pointer to the source memory area
+ * @n: Number of bytes to be copied
+ * Return: Pointer to the destination memory area
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+    unsigned int index;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	return (dest);
+    for (index = 0; index < n; index++)
+        dest[index] = src[index];
+    return (dest);
 }
