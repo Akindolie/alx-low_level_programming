7722a9924e0f52248a2a00a89520403df2135a70
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:37:02 +0100

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,20 +1,21 @@
-/**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: pointer to string to search through
- * @accept: array of bytes to search for
- * Return: pointer to the byte in s that matches one of the bytes in accept
- * or return NULL if no bytes are found.
- */
+#include "main.h"
 
+/**
+ * _strpbrk - Search for a byte in a string that matches one of the bytes in a set.
+ * @s: The string to be searched.
+ * @accept: The set of bytes to compare against.
+ * 
+ * Return: A pointer to the first byte in 's' that matches any byte in 'accept', or NULL if no match is found.
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int search_index;
 
 	while (*s != '\0')
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (search_index = 0; accept[search_index] != '\0'; search_index++)
 		{
-			if (*s == accept[i])
+			if (*s == accept[search_index])
 				return (s);
 		}
 		s++;
