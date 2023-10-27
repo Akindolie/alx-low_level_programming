db49d7dd2cc201014aec613f5d5c9c4fd9a0317f
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:35:26 +0100

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _isalpha - tests whether a character is from the English alphabet.
- * @c: character to be checked.
- * Return: 1 if the character is an English character.
- * 0 if the character is not an English character.
+ * _isalpha - Checks if a character is an English alphabet letter.
+ * @c: The character to be evaluated.
+ * 
+ * Return: 1 if 'c' is an English alphabet letter, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return (1);
+    else
+        return (0);
 }
