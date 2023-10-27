b158d1219f23a56102bd6359d9afe920cbcef14f
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 00:30:33 +0100

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,14 +1,17 @@
-/**
- * _strcmp - compares two strings, ending at the first byte that is different.
- * @s1: string to be compared
- * @s2: string to compare by
- * Return: the difference between the first character that are not the same
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
+#include "main.h"
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
-		;
-	return (s1[i] - s2[i]);
+/**
+ * _strcmp - Compares two strings, stopping at the first differing character.
+ * @str1: The first string to compare.
+ * @str2: The second string to compare against.
+ * Return: The difference between the first differing characters.
+ */
+int _strcmp(char *str1, char *str2)
+{
+    int index;
+
+    for (index = 0; str1[index] != '\0' && str2[index] != '\0' && str1[index] == str2[index]; index++)
+        ;
+
+    return (str1[index] - str2[index]);
 }
