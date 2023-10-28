8e46e5848bfce1646d3973d2ec17e8f11944140d
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-28 21:29:56 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - program prints its own name.
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0 on success
+ * main - Entry point of the program, prints its own name.
+ * @argc: Argument count
+ * @argv: Argument vector
+ * 
+ * Return: Always returns 0 to indicate success.
  */
 int main(int argc, char *argv[])
 {
-	argc--; /*added to pass checks*/
-	printf("%s\n", argv[0]);
-	return (0);
+    argc--; /* Decrement argc to pass checks */
+    printf("%s\n", argv[0]);
+    return (0);
 }
