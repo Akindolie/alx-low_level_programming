9770545e55ade4e189a0439b55e111ae60aca378
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-30 12:07:45 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
@@ -7,9 +8,7 @@
  * @argv: Argument vector
  * Return: Always returns 0 to indicate success.
  */
-int main(int argc, char *argv[])
-{
-      printf("%s\n", argv[0]);
-      return (0);
+int main(int argc, char * argv[]) {
+  printf("%s\n", argv[0]);
+  return (0);
 }
-
