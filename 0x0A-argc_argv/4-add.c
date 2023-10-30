10ad4b853ab66bd30dc79c673c6c00cf07009050
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-30 13:03:11 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
  * main - Add positive numbers.
  * @argc: The argument count.
@@ -9,34 +8,34 @@
  *
  * Return: 0 on success, 1 on error.
  */
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    int sum = 0;
+        int sum = 0;
 
-    if (argc == 1)
-    {
-        /* No numbers passed, print 0 and return. */
-        printf("0\n");
-        return 0;
-    }
-
-    for (int i = 1; i < argc; i++)
-    {
-        for (int j = 0; argv[i][j]; j++)
-	{
-            if (!isdigit(argv[i][j]))
-	    {
-                /* If a non-digit character is found, print Error and return 1. */
-                printf("Error\n");
-                return 1;
-            }
+        if (argc == 1)
+        {
+                /* No numbers passed, print 0 and return. */
+                printf("0\n");
+                return 0;
         }
 
-        /* Convert the valid string to an integer and add it to the sum. */
-        sum += atoi(argv[i]);
-    }
+        for (int i = 1; i < argc; i++)
+        {
+                for (int j = 0; argv[i][j]; j++)
+                {
+                        if (!isdigit(argv[i][j]))
+                        {
+                                /* If a non-digit character is found, print Error and return 1. */
+                                printf("Error\n");
+                                return 1;
+                        }
+                }
 
-    /* Print the sum of positive numbers. */
-    printf("%d\n", sum);
-    return 0;
+                /* Convert the valid string to an integer and add it to the sum. */
+                sum += atoi(argv[i]);
+        }
+
+        /* Print the sum of positive numbers. */
+        printf("%d\n", sum);
+        return 0;
 }
