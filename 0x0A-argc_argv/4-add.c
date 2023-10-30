76181ef5bb3b91742169ef7e5fa15b20c6cb3f1e
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-30 12:42:29 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,40 +2,47 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-/**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
-}
-/**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int main(int argc, char *argv[])
-{
-	int result, i;
 
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	for (i = 1; i < argc; i++)
-		result += atoi(argv[i]);
-	printf("%d\n", result);
-	return (0);
+/**
+ * is_valid_input - checks if the input arguments are valid digits
+ * @argc: the argument count
+ * @index: the current index in argv[]
+ * @char_index: the index for characters in argv[index]
+ * @argv: the argument vector
+ *
+ * Return: 0 on success (valid input), 1 on failure (invalid input)
+ */
+int is_valid_input(int argc, int index, unsigned int char_index, char *argv[]) {
+    for (index = 1; index < argc; index++) {
+        for (char_index = 0; argv[index][char_index] != '\0'; char_index++) {
+            if (!isdigit(argv[index][char_index])) {
+                return 1; // Invalid input
+            }
+        }
+    }
+    return 0; // Valid input
+}
+
+/**
+ * main - adds all valid integer arguments together.
+ * @argc: the argument count, only accepts valid integers separated by spaces.
+ * @argv: the argument vector
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[]) {
+    int sum = 0;
+    int i;
+
+    if (is_valid_input(argc, 1, 0, argv) == 1) {
+        printf("Error\n");
+        return 1; // Invalid input
+    }
+
+    for (i = 1; i < argc; i++) {
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0; // Success
 }
