ca9bea7539e3f64c37b9119f8fdfa3c9aa34736d
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-30 13:07:15 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,28 +15,22 @@ int main(int argc, char *argv[])
 
     if (argc == 1)
     {
-        /* No numbers passed, print 0 and return. */
-        printf("0\n");
-        return 0;
+            printf("0\n");
+            return 0;
     }
 
     for (int i = 1; i < argc; i++)
     {
-        for (int j = 0; argv[i][j]; j++)
-        {
-            if (!isdigit(argv[i][j]))
+            for (int j = 0; argv[i][j]; j++)
             {
-                /* If a non-digit character is found, print Error and return 1. */
-                printf("Error\n");
-                return 1;
+                if (!isdigit(argv[i][j]))
+                {
+                    printf("Error\n");
+                    return 1;
+                }
             }
-        }
-
-        /* Convert the valid string to an integer and add it to the sum. */
-        sum += atoi(argv[i]);
+            sum += atoi(argv[i]);
     }
-
-    /* Print the sum of positive numbers. */
     printf("%d\n", sum);
     return 0;
 }
