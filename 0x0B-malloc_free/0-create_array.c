682d90966ccef93786149c7210bec3386b0dc94b
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-30 14:08:23 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-	unsigned int i;
+	unsigned int index;
 
 	if (size > 0)
 	{
@@ -20,8 +20,8 @@ char *create_array(unsigned int size, char c)
 	else
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		array[i] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
 	return (array);
 }
