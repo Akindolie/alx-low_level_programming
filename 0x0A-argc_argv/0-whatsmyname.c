3eb6a997734a76b93009c06c21d42e079597181d
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-10-30 11:23:18 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  * 
  * Return: Always returns 0 to indicate success.
  */
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-   // argc--; /* Decrement argc to pass checks */
     printf("%s\n", argv[0]);
     return (0);
 }
