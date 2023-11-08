f9577dba1aef4858bad8657150585d22ae712865
Akinloye Michael Akindolie <michaelakindolie@gmail.com>
2023-11-08 13:46:28 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
-  * main - define function
-  * @argc: count of arguments
-  * @argv: pointer to array of pointers
-  * Return: 0 on success, 98 on malformed arguments, 99 if no operator
+  * main - Function
+  * @argc: The number of arguments
+  * @argv: The pointer to array of pointers
+  * Return: Returns 0 for success, 98 for malformed arguments, and 99 in case of no operator.
   */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int n1, n2;
 
 	if (argc != 4)
 		printf("Error\n"), exit(98);
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
 	if (get_op_func(argv[2]) == NULL)
 		printf("Error\n"), exit(99);
-	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+	if (n2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
 		printf("Error\n"), exit(100);
-	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+	printf("%d\n", (get_op_func(argv[2])(n1, n2)));
 	return (0);
 }
